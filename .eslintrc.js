403c3ebeeada9b8d5feed0cad28a2b511162f1ba
Alex Gabites <alex@netspeed.net.nz>
2020-02-26 22:37:51 +1300

    update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['airbnb-typescript/base', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   rules: {
     'import/prefer-default-export': 'off',
   },
